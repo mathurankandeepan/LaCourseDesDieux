65b98263f69b4ec37983560699efcbff7c764e5c
Mathuran Kandeepan <mathuran752000@gmail.com>
2020-09-02 14:22:12 +0200

    Project Final

---
+++ Chevredelamort.java
@@ -0,0 +1,13 @@
+public class Chevredelamort extends Creature{
+  public static final Chevredelamort chevredelamort = new Chevredelamort();
+
+
+  public Chevredelamort (){
+    super("Chevredelamort");
+  }
+
+  public double getVitesse(){
+    return 100000;
+  }
+
+}
---
+++ Sonic.java
@@ -0,0 +1,13 @@
+public class Sonic extends Creature{
+
+  public static final Sonic sonic = new Sonic();
+
+  private Sonic (){
+    super("Sonic");
+  }
+
+  public double getVitesse(){
+    return 50;
+  }
+
+}
